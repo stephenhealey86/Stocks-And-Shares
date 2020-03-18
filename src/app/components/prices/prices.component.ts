37c72df67b219fa6d30652eea67cc5d08e476176
Stephen Healey <s_healey@hotmail.co.uk>
2020-03-18 14:41:14 +0000

    Added auto drop down for smaller screen

--- src/app/components/prices/prices.component.ts
+++ src/app/components/prices/prices.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterContentInit } from '@angular/core';
 import { ExchangeModel } from 'src/app/models/exchange-model';
 import { FinnhubPricesService } from 'src/app/services/finnhub-prices.service';
 import { SymbolModel } from 'src/app/models/symbol-model';
 import { FormControl } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 import { QuoteModel } from 'src/app/models/quote-model';
-import { CandleModel } from 'src/app/models/candle-model';
 
 @Component({
   selector: 'app-prices',
   templateUrl: './prices.component.html',
   styleUrls: ['./prices.component.css']
 })
-export class PricesComponent implements OnInit {
+export class PricesComponent implements OnInit, AfterContentInit {
 
   exchanges: Array<ExchangeModel>;
   exchange = {} as ExchangeModel;
@@ -36,6 +35,24 @@ export class PricesComponent implements OnInit {
   ngOnInit() {
     this.getExchanges();
     this.initialiseSearchBar();
+  }
+
+  ngAfterContentInit(): void {
+    window.addEventListener('resize', () => {
+      this.detectScreenSize();
+    });
+    setTimeout(() => {
+      this.detectScreenSize();
+    }, 500);
+  }
+
+  private detectScreenSize() {
+    if (window.innerWidth < 550) {
+      const DROP_DOWN_DIV = document.getElementById('navbarSupportedContent') as HTMLDivElement;
+      if (DROP_DOWN_DIV !== null && this.exchange.name === undefined) {
+        DROP_DOWN_DIV.classList.add('show');
+      }
+    }
   }
 
   private getExchanges(): void {
